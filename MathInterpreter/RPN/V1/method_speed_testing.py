70f507d6a14a7d1d0a9145bfbc99a6a66358ec99
Ben Maltby <91958897+rChantx86@users.noreply.github.com>
2022-08-31 12:29:11 +0100

    Delete MathInterpreter/RPN/V1 directory

--- MathInterpreter/RPN/V1/method_speed_testing.py
+++
@@ -1,9 +0,0 @@
-import time
-
-list1 = ['a', 'b', 'c']
-list2 = [1, 2, 3]
-
-if __name__ == "__main__":
-	startTime = time.time()
-	list1 = list1.extend(list2)
-	print("Time:",time.time()-startTime)
